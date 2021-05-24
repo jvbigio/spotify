907c285f3cf07a19d5d73b349bdf9a0cb84ec689
Joe Bigio <jvbigio@gmail.com>
2021-05-24 16:36:32 -0400

    working on making selected page active highlight

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,28 +1,23 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
 import './Navigation.css'
+import Navbar from 'react-bootstrap/Navbar'
+import Nav from 'react-bootstrap/Nav'
+import { FaMusic } from 'react-icons/fa'
 
 const Navigation = () => {
   return (
-    <nav className='navbar navbar-expand-lg navbar-dark bg-dark'>
-      <a className='navbar-brand' href='/'>Spotify</a>
-      <button className='navbar-toggler' type='button' data-toggle='collapse' data-target='#navbarNav' aria-controls='navbarNav' aria-expanded='false' aria-label='Toggle navigation'>
-        <span className='navbar-toggler-icon' />
-      </button>
-      <div className='collapse navbar-collapse' id='navbarNav'>
-        <ul className='navbar-nav'>
-          <li className='nav-item active'>
-            <a className='nav-link' href='/'>Home <span className='sr-only'>(current)</span></a>
-          </li>
-          <li className='nav-item'>
-            <a className='nav-link' href='/search'>Search</a>
-          </li>
-          <li className='nav-item'>
-            <a className='nav-link' href='/random'>Random</a>
-          </li>
-        </ul>
-      </div>
-    </nav>
+    <Navbar bg='dark' expand='lg' variant='dark'>
+      <Navbar.Brand href='/'><FaMusic /></Navbar.Brand>
+      <Navbar.Toggle aria-controls='basic-navbar-nav' />
+      <Navbar.Collapse id='basic-navbar-nav'>
+        <Nav className='mr-auto'>
+          <Nav.Link href='/'>Home</Nav.Link>
+          <Nav.Link href='/search'>Search</Nav.Link>
+          <Nav.Link href='/random'>Random</Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
   )
 }
 
