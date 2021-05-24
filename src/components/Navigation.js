0ff21b033704ad447754fd6342574ee5bb4e206b
Joe Bigio <jvbigio@gmail.com>
2021-05-24 17:03:32 -0400

    Still working on active link css

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,24 +1,38 @@
 import React from 'react'
-import { NavLink } from 'react-router-dom'
+import { NavLink, Link } from 'react-router-dom'
 import './Navigation.css'
-import Navbar from 'react-bootstrap/Navbar'
-import Nav from 'react-bootstrap/Nav'
+import { Navbar, Nav } from 'react-bootstrap'
 import { FaMusic } from 'react-icons/fa'
 
 const Navigation = () => {
   return (
-    <Navbar bg='dark' expand='lg' variant='dark'>
+    <Navbar className='d-flex justify-content-around' bg='dark' expand='lg' variant='dark'>
       <Navbar.Brand href='/'><FaMusic /></Navbar.Brand>
       <Navbar.Toggle aria-controls='basic-navbar-nav' />
       <Navbar.Collapse id='basic-navbar-nav'>
         <Nav className='mr-auto'>
-          <Nav.Link href='/'>Home</Nav.Link>
-          <Nav.Link href='/search'>Search</Nav.Link>
-          <Nav.Link href='/random'>Random</Nav.Link>
+          <NavLink to='/' activeClassName='active'>Home</NavLink>
+          <NavLink to='/search' activeClassName='active'>Search</NavLink>
+          <NavLink to='/random' activeClassName='active'>Random</NavLink>
         </Nav>
       </Navbar.Collapse>
     </Navbar>
   )
 }
+// const Navigation = () => {
+//   return (
+//     <Navbar bg='dark' expand='lg' variant='dark'>
+//       <Navbar.Brand href='/'><FaMusic /></Navbar.Brand>
+//       <Navbar.Toggle aria-controls='basic-navbar-nav' />
+//       <Navbar.Collapse id='basic-navbar-nav'>
+//         <Nav className='mr-auto'>
+//           <Nav.Link href='/'>Home</Nav.Link>
+//           <Nav.Link href='/search'>Search</Nav.Link>
+//           <Nav.Link href='/random'>Random</Nav.Link>
+//         </Nav>
+//       </Navbar.Collapse>
+//     </Navbar>
+//   )
+// }
 
 export default Navigation
