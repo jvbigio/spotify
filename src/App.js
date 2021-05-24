6b13cb179ac810bd7be1f3972cca0c3d014ebc6d
Joe Bigio <jvbigio@gmail.com>
2021-05-24 14:35:02 -0400

    Added react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import './App.css';
+import './App.css'
 
-function App() {
+function App () {
   return (
-    <div className="App">
-     <h1>Spotify App</h1>
+    <div className='App'>
+      <h1>Spotify App</h1>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
