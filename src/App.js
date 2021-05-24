e715dae795f513ec8a48ccc66cd171961d7474ff
Joe Bigio <jvbigio@gmail.com>
2021-05-24 15:26:28 -0400

    Created navbar

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import Home from './pages/Home'
 import Search from './pages/Search'
 import Random from './pages/Random'
+import Navigation from './components/Navigation'
 
 function App () {
   return (
     <Router>
       <div className='App'>
+        <Navigation />
         <main className='card'>
           <Switch>
             <Route path='/' exact component={Home} />
