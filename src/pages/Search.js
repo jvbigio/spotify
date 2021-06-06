34c12566b2d85a5522d8af5f64386560f9f519d6
Joe Bigio <jvbigio@gmail.com>
2021-06-05 22:49:46 -0400

    Working on Search component

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,9 +1,17 @@
 import React from 'react'
+import './Search.css'
+import Form from 'react-bootstrap/Form'
+import { BsSearch } from 'react-icons/bs'
 
 const Search = () => {
   return (
-    <div>
-      <h1>Search Page</h1>
+    <div className='container'>
+      <Form className='input-search justify-content-center'>
+        <Form.Group controlId='form-search' autoComplete='off'>
+          <span><BsSearch /></span>
+          <Form.Control type='text' placeholder='Artist, album, or song' />
+        </Form.Group>
+      </Form>
     </div>
   )
 }
