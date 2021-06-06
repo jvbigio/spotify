49cf2e1678b62b80eba02e48342e014d1009e5ed
Joe Bigio <jvbigio@gmail.com>
2021-06-05 21:40:42 -0400

    Added homepage content

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,7 +4,10 @@ import './Home.css'
 const Home = () => {
   return (
     <div className='container-fluid'>
-      <h1>Homepage</h1>
+      <h1 className='title'>Welcome to Spotify Clone</h1>
+      <main>
+        <p>This app allows a user to search for an artist, album or song. Users can save 5 of their favorite music artists and generate a random song from these musicians.</p>
+      </main>
     </div>
   )
 }
