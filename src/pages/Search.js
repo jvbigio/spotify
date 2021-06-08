e2b67e13ada1a13d5a072c0600e4ddb409119e34
Joe Bigio <jvbigio@gmail.com>
2021-06-07 21:04:33 -0400

    Added bg image to Search page

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,21 +1,27 @@
 import React from 'react'
 import './Search.css'
-import { Form, InputGroup } from 'react-bootstrap'
+import { Form, InputGroup, Image } from 'react-bootstrap'
 import { BsSearch } from 'react-icons/bs'
+import imageBG from '../images/music-chord-bg.jpg'
 
 const Search = () => {
   return (
     <div className='container'>
-      <Form className='input-search justify-content-center'>
-        <Form.Group controlId='form-search' autoComplete='off'>
-          <InputGroup>
-            <InputGroup.Prepend>
-              <InputGroup.Text id='form-icon'><BsSearch /></InputGroup.Text>
-            </InputGroup.Prepend>
-            <Form.Control type='text' placeholder='Artist, album, or song' />
-          </InputGroup>
-        </Form.Group>
-      </Form>
+      <div className='search-form'>
+        <Form className='input-search justify-content-center'>
+          <Form.Group controlId='form-search' autoComplete='off'>
+            <InputGroup>
+              <InputGroup.Prepend>
+                <InputGroup.Text id='form-icon'><BsSearch /></InputGroup.Text>
+              </InputGroup.Prepend>
+              <Form.Control type='text' placeholder='Artist, album, or song' />
+            </InputGroup>
+          </Form.Group>
+        </Form>
+      </div>
+      <main className='content'>
+        {/* <Image className='bg' src={imageBG} fluid /> */}
+      </main>
     </div>
   )
 }
