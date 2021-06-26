ad3de3b819b249287fbd2a8ce68b616dc2b2a989
Joe Bigio <jvbigio@gmail.com>
2021-06-26 12:50:22 -0400

    Deleted comment in App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import Home from './pages/Home'
 import Search from './pages/Search'
 import Random from './pages/Random'
-// import Navigation from './components/Navigation'
 import Sidebar from './components/Sidebar'
 
 function App () {
