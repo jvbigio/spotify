83323d3e43e6205d835c2e9b2cfb2d6f5b34f0b1
Joe Bigio <jvbigio@gmail.com>
2021-07-01 02:36:41 -0400

    Working on bearer token

--- src/controllers/token-service.js
+++ src/controllers/token-service.js
@@ -1,10 +1,36 @@
 const axios = require('axios').default
-// require('dotenv').config()
-// const dotenv = require('dotenv')
+require('dotenv').config()
+
+let bearer
 
 const getAuthToken = () => {
   // console.log('testing!!') // works
   // console.log(process.env.API_KEY)
+  try {
+    if (bearer) return bearer
+    const URL = 'https://accounts.spotify.com/api/token'
+
+    const config = {
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded'
+      },
+      auth: {
+        username: process.env.API_KEY,
+        password: process.env.API_SECRET_KEY
+      }
+    }
+
+    return axios
+      .post(URL, 'grant_type=client_credentials', config)
+      .then(response => {
+        axios.defaults.headers.common = {
+          Authorization: `Bearer ${response.data.access_token}`
+        }
+        return response.data.access_token
+      })
+  } catch (err) {
+    console.log(err)
+  }
 }
 
 module.exports = {
