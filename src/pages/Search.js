67ab939092366cf60be337f094e96c200e01f40e
Joe Bigio <jvbigio@gmail.com>
2021-07-03 04:44:36 -0400

    Response data successfully returning

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -5,23 +5,35 @@ import { BsSearch } from 'react-icons/bs'
 import imageBG from '../images/music-chord-bg.jpg'
 
 import authToken from '../controllers/token-service'
+import axios from 'axios'
 
 authToken.getAuthToken()
 
 const Search = () => {
-  // debugger
-  const [search, setSearch] = useState('')
+  const [searchInput, setSearchInput] = useState('')
+  const [searchResults, setSearchResults] = useState([])
 
-  const getUserInput = e => setSearch(e.target.value)
+  const getUserInput = e => setSearchInput(e.target.value)
 
   const handleSearch = async (e) => {
     e.preventDefault()
+
+    // const artistUrl = `https://api.spotify.com/v1/search?query=${searchInput}&type=artist`
+    // const albumUrl = `https://api.spotify.com/v1/search?query=${searchInput}&type=album`
+
+    try {
+      const response = await axios.get(`https://api.spotify.com/v1/search?query=${searchInput}&type=artist`)
+      console.log(response.data)
+    } catch (err) {
+      console.log(err)
+    }
+    setSearchInput('')
   }
 
   return (
     <div className='search-container'>
       <div className='search-form'>
-        <Form onSubmit={handleSearch} className='input-search justify-content-center w-50'>
+        <Form onSubmit={handleSearch} className='input-search justify-content-center w-50' autoComplete='off'>
           <Form.Group controlId='form-search' autoComplete='off'>
             <InputGroup>
               <InputGroup.Prepend>
@@ -30,6 +42,7 @@ const Search = () => {
               <Form.Control
                 type='text'
                 placeholder='Artist, album, or song'
+                value={searchInput}
                 onChange={(e) => getUserInput(e)}
               />
             </InputGroup>
