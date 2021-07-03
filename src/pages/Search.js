87a947d74bb20523a9c1da56faaed3804bea5be6
Joe Bigio <jvbigio@gmail.com>
2021-07-03 03:19:01 -0400

    Added state to Search component. Working on user input

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Search.css'
 import { Form, InputGroup, Image } from 'react-bootstrap'
 import { BsSearch } from 'react-icons/bs'
@@ -9,16 +9,29 @@ import authToken from '../controllers/token-service'
 authToken.getAuthToken()
 
 const Search = () => {
+  // debugger
+  const [search, setSearch] = useState('')
+
+  const getUserInput = e => setSearch(e.target.value)
+
+  const handleSearch = async (e) => {
+    e.preventDefault()
+  }
+
   return (
     <div className='search-container'>
       <div className='search-form'>
-        <Form className='input-search justify-content-center w-50'>
+        <Form onSubmit={handleSearch} className='input-search justify-content-center w-50'>
           <Form.Group controlId='form-search' autoComplete='off'>
             <InputGroup>
               <InputGroup.Prepend>
                 <InputGroup.Text id='form-icon'><BsSearch /></InputGroup.Text>
               </InputGroup.Prepend>
-              <Form.Control type='text' placeholder='Artist, album, or song' />
+              <Form.Control
+                type='text'
+                placeholder='Artist, album, or song'
+                onChange={(e) => getUserInput(e)}
+              />
             </InputGroup>
           </Form.Group>
         </Form>
