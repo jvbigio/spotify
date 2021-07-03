4baff54aef4bafaa1c2ad83d6c87849a59f97f1f
Joe Bigio <jvbigio@gmail.com>
2021-07-03 04:54:20 -0400

    Changed homepage intro

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,7 +5,7 @@ const Home = () => {
   return (
     // <div className='container-fluid home-card'>
     <div className='container home-card'>
-      <h1 className='title'>Welcome to Spotify Clone</h1>
+      <h1 className='title'>Welcome to Spotify 2.0</h1>
       <main>
         <p>This app allows a user to search for an artist, album or song. Users can save 5 of their favorite music artists and generate a random song from these musicians.</p>
       </main>
