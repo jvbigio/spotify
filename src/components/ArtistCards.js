775b2d2f2340b64766a2559412e088424abd6b2a
Joe Bigio <jvbigio@gmail.com>
2021-07-16 01:22:37 -0400

    Working on css grid for cards

--- src/components/ArtistCards.js
+++ src/components/ArtistCards.js
@@ -7,11 +7,11 @@ import { Card, Container, Row, Col, Image } from 'react-bootstrap'
 
 const ArtistCards = ({ name, artistPic, handleClick }) => {
   return (
-    <div className='container-fluid'>
+    <div className='cards'>
       {/* <Card className='artist-card' onClick={(e) => handleClick(e)}> */}
       <Card className='artist-card'>
-        <img className='artist-img rounded-circle' src={artistPic} alt='artist' height='150px' width='150px' />
-        {/* <Image src={artistPic} className='artistImg rounded-circle' fluid /> */}
+        {/* <img className='artist-img rounded-circle' src={artistPic} alt='artist' height='150px' width='150px' /> */}
+        <Image src={artistPic} className='artist-img rounded-circle' fluid />
         <Card.Body>
           <Card.Text>
             {/* {name} */}
