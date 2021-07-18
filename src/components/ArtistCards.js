3bf652f7400d543fbe510206996df886503332fa
Joe Bigio <jvbigio@gmail.com>
2021-07-18 14:08:44 -0400

    added external link to spotify when clicking image profile

--- src/components/ArtistCards.js
+++ src/components/ArtistCards.js
@@ -1,20 +1,19 @@
 import React from 'react'
 import './ArtistCards.css'
 
-import { Card, Container, Row, Col, Image } from 'react-bootstrap'
+import { Card, Image } from 'react-bootstrap'
 
 import music from '../images/flat-music.png'
 
 const ArtistCards = ({ artist, artistPic, handleClick }) => {
   console.log(artist.external_urls)
-  const artistMedia = artist.images[0] // shows array of images
+  const artistMedia = artist.images[0]
   const hasMedia = () => artistMedia ? artistMedia.url : music
 
   return (
     <div className='cards'>
       <Card className='artist-card' onClick={(e) => handleClick(e)}>
-        {/* <a href={artistMedia} target='_blank' rel='noreferrer'> */}
-        <a href={(artistMedia) ? artist.external_urls : null} target='_blank' rel='noreferrer'>
+        <a href={(artistMedia) ? artist.external_urls.spotify : null} target='_blank' rel='noreferrer'>
           <Image className='artist-img rounded-circle w-75 h-75' src={hasMedia()} fluid />
         </a>
         <Card.Body>
