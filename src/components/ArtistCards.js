3f325ac9a035880e7007f7a63c7abd59479b60af
Joe Bigio <jvbigio@gmail.com>
2021-07-23 05:59:13 -0400

    Fixed margin right for content wrapper

--- src/components/ArtistCards.js
+++ src/components/ArtistCards.js
@@ -6,8 +6,6 @@ import { Card, Image } from 'react-bootstrap'
 import music from '../images/flat-music.png'
 
 const ArtistCards = ({ artist, artistPic, handleClick, dropDownValue }) => {
-  // console.log(artist.external_urls)
-  // console.log(dropDownValue)
   const artistMedia = artist.images[0]
   const hasMedia = () => artistMedia ? artistMedia.url : music
 
@@ -15,7 +13,6 @@ const ArtistCards = ({ artist, artistPic, handleClick, dropDownValue }) => {
     <div className='artist-cards'>
       <Card className='artist-card' onClick={(e) => handleClick(e)}>
         <a href={(artistMedia) ? artist.external_urls.spotify : null} target='_blank' rel='noreferrer'>
-          {/* <Image className='artist-img rounded-circle w-75 h-75' src={hasMedia()} fluid /> */}
           <Image className='rounded-circle mt-2 w-100 h-100 p-1' id='artist-img' src={hasMedia()} fluid />
         </a>
         <Card.Body>
