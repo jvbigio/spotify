66156ddbc1124e65decf037a13df6b8a0908c172
Joe Bigio <jvbigio@gmail.com>
2021-07-23 01:58:32 -0400

    Added as div to Card.Text

--- src/components/SongCards.js
+++ src/components/SongCards.js
@@ -6,7 +6,7 @@ import { Card, Image } from 'react-bootstrap'
 import music from '../images/flat-music.png'
 
 const SongCards = ({ artist, artistPic, handleClick, dropDownValue }) => {
-  // console.log(dropDownValue)
+  console.log(artist.duration_ms)
   // response.data.tracks.items
   // console.log(artist.album.images[2])
   const artistMedia = artist.album.images[2]
@@ -14,20 +14,19 @@ const SongCards = ({ artist, artistPic, handleClick, dropDownValue }) => {
 
   return (
     <div className='song-cards'>
-      {/* <div id='song-cards'> */}
       <Card className='song-card' onClick={(e) => handleClick(e)}>
-        {/* <Card id='song-card' onClick={(e) => handleClick(e)}> */}
-        {/* <Card className={`song-card ${dropDownValue === 'track' ? 'active' : null}`} onClick={(e) => handleClick(e)}> */}
         <a href={(artistMedia) ? artist.external_urls.spotify : null} target='_blank' rel='noreferrer'>
-          {/* <Image className='song-img rounded-circle w-25 h-25' id='song-img' src={hasMedia()} fluid /> */}
           <Image className='song-img' id='song-img' src={hasMedia()} fluid />
         </a>
         <Card.Body>
-          <Card.Text>
+          <Card.Text as='div'>
             {artist.name}
           </Card.Text>
-          <Card.Text className='secondary-text'>
-            {artist.artists.name}
+          <Card.Text className='secondary-text' as='div'>
+            {artist.album.name}
+          </Card.Text>
+          <Card.Text className='secondary-text' as='div'>
+            {artist.duration_ms}
           </Card.Text>
         </Card.Body>
       </Card>
