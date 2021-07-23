9906633ba0eafa2952aac0c8a9ee535a527df3cb
Joe Bigio <jvbigio@gmail.com>
2021-07-23 06:22:03 -0400

    fixed padding top of cards

--- src/components/AlbumCards.js
+++ src/components/AlbumCards.js
@@ -6,7 +6,6 @@ import { Card, Image } from 'react-bootstrap'
 import music from '../images/flat-music.png'
 
 const AlbumCards = ({ artist, artistPic, handleClick, dropDownValue }) => {
-  // console.log(dropDownValue)
   const artistMedia = artist.images[0]
   const hasMedia = () => artistMedia ? artistMedia.url : music
 
@@ -14,9 +13,9 @@ const AlbumCards = ({ artist, artistPic, handleClick, dropDownValue }) => {
     <div className='cards'>
       <Card className='album-card' onClick={(e) => handleClick(e)}>
         <a href={(artistMedia) ? artist.external_urls.spotify : null} target='_blank' rel='noreferrer'>
-          <Image className='artist-img rounded-circle w-75 h-75' src={hasMedia()} fluid />
+          <Image className='artist-img rounded-circle w-90 h-75' src={hasMedia()} fluid />
         </a>
-        <Card.Body>
+        <Card.Body className='pt-0'>
           <Card.Text>
             {artist.name}
           </Card.Text>
