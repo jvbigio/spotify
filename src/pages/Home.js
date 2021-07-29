0767a89c4c4e5685af5405fa2d1ea12c12fe1bc8
Joe Bigio <jvbigio@gmail.com>
2021-07-29 01:24:00 -0400

    working on image on homepage

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,58 +2,17 @@ import React from 'react'
 import './Home.css'
 import { Image, Carousel } from 'react-bootstrap'
 
-// const Home = () => {
-//   return (
-//     <div className='container home-card'>
-//       <h1 className='title'>Welcome to Spotify 2.0</h1>
-//       <main>
-//         <p>This app allows a user to search for an artist, album or song. Users can save 5 of their favorite music artists and generate a random song from these musicians.</p>
-//       </main>
-//     </div>
-//   )
-// }
+import vinyl from '../images/vinyl-record.jpg'
+
 const Home = () => {
   return (
     <div className='container home-card'>
       <h1 className='title'>Welcome to Spotify 2.0</h1>
-      <div className='slide-container'>
-        <Carousel>
-          <Carousel.Item>
-            <img
-              className='d-block w-100'
-              src='holder.js/800x400?text=First slide&bg=373940'
-              alt='First slide'
-            />
-            <Carousel.Caption>
-              <h3>First slide label</h3>
-              <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-            </Carousel.Caption>
-          </Carousel.Item>
-          <Carousel.Item>
-            <img
-              className='d-block w-100'
-              src='holder.js/800x400?text=Second slide&bg=282c34'
-              alt='Second slide'
-            />
-
-            <Carousel.Caption>
-              <h3>Second slide label</h3>
-              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-            </Carousel.Caption>
-          </Carousel.Item>
-          <Carousel.Item>
-            <img
-              className='d-block w-100'
-              src='holder.js/800x400?text=Third slide&bg=20232a'
-              alt='Third slide'
-            />
-
-            <Carousel.Caption>
-              <h3>Third slide label</h3>
-              <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-            </Carousel.Caption>
-          </Carousel.Item>
-        </Carousel>
+      <div className='intro'>
+        <p>This app allows a user to search for an artist, album or song. Users can save 5 of their favorite music artists and generate a random song from these musicians.</p>
+      </div>
+      <div className='image-container'>
+        <Image src={vinyl} fluid />
       </div>
     </div>
   )
