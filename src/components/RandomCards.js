5105a0cc3a054528d30e549653c9dd06a02381c2
Joe Bigio <jvbigio@gmail.com>
2021-07-31 11:46:41 -0400

    refactored RandomCards using artistCards as template

---
+++ src/components/RandomCards.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import './RandomCards.css'
+
+import { Card, Image } from 'react-bootstrap'
+
+const RandomCards = ({ name, imageSrc, handleClick }) => {
+  return (
+    <div className='random-cards'>
+      <Card className='random-card' onClick={(e) => handleClick(e)}>
+        <Image className='rounded-circle mt-2 w-100 h-100 p-1' src={imageSrc} fluid />
+        <Card.Body>
+          <Card.Text>
+            {name}
+          </Card.Text>
+        </Card.Body>
+      </Card>
+    </div>
+  )
+}
+
+export default RandomCards
