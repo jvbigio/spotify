3b4643e3556b79f934ace0981ee1667b82e50da0
Joe Bigio <jvbigio@gmail.com>
2021-07-31 11:53:11 -0400

    added state to Random.js

--- src/pages/Random.js
+++ src/pages/Random.js
@@ -1,8 +1,22 @@
 import React, { useState } from 'react'
 import './Random.css'
-// import ArtistCards from '../components/ArtistCards'
+import RandomCards from '../components/RandomCards'
+
+import drake from '../images/drake.jpeg'
+import linkinPark from '../images/linkin-park.jpeg'
+import metallica from '../images/metallica.jpeg'
+import theWeeknd from '../images/the-weeknd.jpeg'
+import tiesto from '../images/tiesto.jpeg'
 
 const Random = () => {
+  const [favArtists, setFavArtists] = useState([
+    { name: 'Drake', imageSrc: drake },
+    { name: 'Linkin Park', imageSrc: linkinPark },
+    { name: 'Metallica', imageSrc: metallica },
+    { name: 'The Weeknd', imageSrc: theWeeknd },
+    { name: 'Tiesto', imageSrc: tiesto }
+  ])
+
   return (
     <div />
   )
