712c82b2eec99910f273dd8087c6d8da6cb16375
Joe Bigio <jvbigio@gmail.com>
2021-08-03 06:08:15 -0600

    added id to favArtist state in Random

--- src/pages/Random.js
+++ src/pages/Random.js
@@ -2,6 +2,8 @@ import React, { useState } from 'react'
 import './Random.css'
 import RandomCards from '../components/RandomCards'
 
+import axios from 'axios'
+
 import drake from '../images/drake.jpeg'
 import linkinPark from '../images/linkin-park.jpeg'
 import metallica from '../images/metallica.jpeg'
@@ -10,24 +12,34 @@ import tiesto from '../images/tiesto.jpeg'
 
 const Random = () => {
   const [favArtists, setFavArtists] = useState([
-    { name: 'Drake', imageSrc: drake },
-    { name: 'Linkin Park', imageSrc: linkinPark },
-    { name: 'Metallica', imageSrc: metallica },
-    { name: 'The Weeknd', imageSrc: theWeeknd },
-    { name: 'Tiesto', imageSrc: tiesto }
+    { id: '3TVXtAsR1Inumwj472S9r4', name: 'Drake', imageSrc: drake },
+    { id: '6XyY86QOPPrYVGvF9ch6wz', name: 'Linkin Park', imageSrc: linkinPark },
+    { id: '2ye2Wgw4gimLv2eAKyk1NB', name: 'Metallica', imageSrc: metallica },
+    { id: '1Xyo4u8uXC1ZmMpatF05PJ', name: 'The Weeknd', imageSrc: theWeeknd },
+    { id: '2o5jDhtHVPhrJdv3cEQ99Z', name: 'Tiesto', imageSrc: tiesto }
   ])
 
   const [randomSong, setRandomSong] = useState([])
 
-  const handleClick = name => console.log(name)
+  const handleClick = async (name) => {
+    // console.log(name)
+    try {
+      await axios.get(`https://api.spotify.com/v1/search?q=${name}&type=track&limit=1`)
+        .then(response => {
+          setRandomSong(response.data.artists.items)
+          console.log(randomSong)
+        })
+    } catch (err) {
+      console.log(err)
+    }
+  }
 
   const getRandomCards = favArtists.map(artist => {
     return (
       <RandomCards
-        key={artist.name}
+        key={artist.id}
         name={artist.name}
         imageSrc={artist.imageSrc}
-        // ??
         handleClick={() => handleClick(artist.name)}
       />
     )
