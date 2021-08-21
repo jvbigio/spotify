f9559700694c47ca179df5082584ec3cc5e3b23b
Joe Bigio <jvbigio@gmail.com>
2021-08-21 13:39:39 -0400

    Changed homepage img galleries. Added onClick links. Deleted grid-img-7

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -9,7 +9,6 @@ import album3 from '../images/grid-img-3.png'
 import album4 from '../images/grid-img-4.png'
 import album5 from '../images/grid-img-5.png'
 import album6 from '../images/grid-img-6.png'
-import album7 from '../images/grid-img-7.png'
 
 const Home = () => {
   return (
@@ -20,22 +19,52 @@ const Home = () => {
       </div>
       <div className='gallery'>
         <figure className='gallery__item gallery__item--1'>
-          <img src={album1} className='gallery__img' alt='album cover 1' />
+          <img
+            src={album1}
+            className='gallery__img'
+            alt='album cover 1'
+            onClick={() => window.open('https://open.spotify.com/album/57bABnvvPfNhBQRI70dqlU', '_blank')}
+          />
         </figure>
         <figure className='gallery__item gallery__item--2'>
-          <img src={album2} className='gallery__img' alt='album cover 2' />
+          <img
+            src={album2}
+            className='gallery__img'
+            alt='album cover 2'
+            onClick={() => window.open('https://open.spotify.com/playlist/37i9dQZF1DX82pCGH5USnM', '_blank')}
+          />
         </figure>
         <figure className='gallery__item gallery__item--3'>
-          <img src={album3} className='gallery__img' alt='album cover 3' />
+          <img
+            src={album3}
+            className='gallery__img'
+            alt='album cover 3'
+            onClick={() => window.open('https://open.spotify.com/album/2pKw6GERJVAD61449B1EEM', '_blank')}
+          />
         </figure>
         <figure className='gallery__item gallery__item--4'>
-          <img src={album4} className='gallery__img' alt='album cover 4' />
+          <img
+            src={album4}
+            className='gallery__img'
+            alt='album cover 4'
+            onClick={() => window.open('https://open.spotify.com/album/5Nrx0GEDuLwrQcEvb1IM8l', '_blank')}
+          />
         </figure>
         <figure className='gallery__item gallery__item--5'>
-          <img src={album5} className='gallery__img' alt='album cover 5' />
+          <img
+            src={album5}
+            className='gallery__img'
+            alt='album cover 5'
+            onClick={() => window.open('https://open.spotify.com/album/481JiVXZYUlTtqlv9ewJXV', '_blank')}
+          />
         </figure>
         <figure className='gallery__item gallery__item--6'>
-          <img src={album6} className='gallery__img' alt='album cover 6' />
+          <img
+            src={album6}
+            className='gallery__img'
+            alt='album cover 6'
+            onClick={() => window.open('https://open.spotify.com/album/6QdCohkHKNTVoaSx1ZzitH', '_blank')}
+          />
         </figure>
       </div>
     </div>
