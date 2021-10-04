43b182bca1c5b0292ee3d26ca22638c3243a5d1e
Joe Bigio <jvbigio@gmail.com>
2021-10-04 15:26:29 -0400

    Deleted comments in Random

--- src/pages/Random.js
+++ src/pages/Random.js
@@ -7,21 +7,7 @@ import { FAV_ARTISTS } from '../artists'
 
 import axios from 'axios'
 
-// import drake from '../images/drake.jpeg'
-// import linkinPark from '../images/linkin-park.jpeg'
-// import metallica from '../images/metallica.jpeg'
-// import theWeeknd from '../images/the-weeknd.jpeg'
-// import tiesto from '../images/tiesto.jpeg'
-
 const Random = () => {
-  // const FAV_ARTISTS = [
-  //   { id: '3TVXtAsR1Inumwj472S9r4', name: 'Drake', imageSrc: drake },
-  //   { id: '6XyY86QOPPrYVGvF9ch6wz', name: 'Linkin Park', imageSrc: linkinPark },
-  //   { id: '2ye2Wgw4gimLv2eAKyk1NB', name: 'Metallica', imageSrc: metallica },
-  //   { id: '1Xyo4u8uXC1ZmMpatF05PJ', name: 'The Weeknd', imageSrc: theWeeknd },
-  //   { id: '2o5jDhtHVPhrJdv3cEQ99Z', name: 'Tiesto', imageSrc: tiesto }
-  // ]
-
   const [randomSong, setRandomSong] = useState([])
 
   const handleClick = async (id) => {
